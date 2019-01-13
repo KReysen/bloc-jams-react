b43bfcae00c000ed56e8ad2731bbde0b654ade45
KReysen <43965763+KReysen@users.noreply.github.com>
2019-01-13 15:23:27 -0700

    Import the Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  , document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
+
+// note- serviceworker was unregistered by default when I created the app, but
+// in Bloc routing assignment, it is registered (and formatted differently)
+// I changed it to registered but followed the original formatting
