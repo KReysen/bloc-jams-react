91af048de91e89082ff91cd5803cb4f051a8327f
KReysen <43965763+KReysen@users.noreply.github.com>
2019-01-28 21:32:52 -0700

    Experimenting with bootstrap

--- src/App.js
+++ src/App.js
@@ -4,23 +4,29 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
+import { Navbar } from 'react-bootstrap';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <nav>
+          <Navbar className="Navbar">
             <Link to='/'>Landing</Link>
+
             <Link to='/library'>Library</Link>
-          </nav>
+          </Navbar>
+
           <h1>Bloc Jams</h1>
         </header>
+        <div id="background">
         <main>
            <Route exact path="/" component={Landing} />
            <Route path="/library" component={Library} />
            <Route path="/album/:slug" component={Album} />
         </main>
+      </div>
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,25 +1,29 @@
 import React from 'react';
-
+import { Row, Col } from 'react-bootstrap';
+import '../styles/Landing.css'
 const Landing = () => (
+
+  <div id="landingpage">
   <section className ="landing">
     <h1 className="hero-title">Turn the music up!</h1>
 
-  <section className="selling-points">
-    <div className="point">
+  <Row className="selling-points">
+    <Col md={4} className="point">
       <h2 className="point-title">Choose your music</h2>
       <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
-    </div>
-    <div className="point">
+    </Col>
+    <Col md={4} className="point">
       <h2 className="point-title">Unlimited, streaming, ad-free</h2>
       <p className="point-description">No arbitrary limits. No distractions.</p>
-    </div>
-    <div className="point">
+    </Col>
+    <Col md={4} className="point">
       <h2 className="point-title">Mobile enabled</h2>
       <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-        
-    </div>
+
+    </Col>
+  </Row>
   </section>
-  </section>
+  </div>
 );
 
 export default Landing;
