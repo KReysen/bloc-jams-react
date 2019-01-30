d72388b54143cdf9d8ddb5b80bd8f49b96282e4b
KReysen <43965763+KReysen@users.noreply.github.com>
2019-01-30 15:58:29 -0700

    Started styling library page

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
 import PlayerBar from './PlayerBar';
+import '../styles/Album.css';
 
 class Album extends Component {
   constructor(props) {
@@ -154,16 +155,20 @@ componentWillUnmount() {
       </tr>
     );
     return (
+      <div className="container">
       <section className="album">
+
         <section id="album-info">
-          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+          <img  id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
             <div id="release-info">{this.state.album.releaseInfo}</div>
           </div>
         </section>
-        <table id="song-list">
+
+      
+        <table className="table" id="song-list" >
           <colgroup>
             <col id="song-number-column" />
             <col id="song-title-column" />
@@ -188,6 +193,7 @@ componentWillUnmount() {
 
          />
         </section>
+        </div>
     );
   }
 }
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
+import '../styles/Library.css';
 
 class Library extends Component {
   constructor(props) {
@@ -10,18 +11,23 @@ class Library extends Component {
 
   render() {
     return (
+
       <section className='library'>
         {
           this.state.albums.map( (album, index) =>
+          <div className='eachAlbum'>
             <Link to={`/album/${album.slug}`} key={index} >
-              <img src={album.albumCover} alt={album.title} />
+              <img className="img img-thumbnail" id="albumCover" src={album.albumCover} alt={album.title} />
               <div>{album.title}</div>
               <div>{album.artist}</div>
               <div>{album.songs.length} songs</div>
             </Link>
+            </div>
           )
+
         }
       </section>
+
     );
   }
 }
